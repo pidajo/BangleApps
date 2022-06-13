67c471f52662c70ad39b360ad3ddfcb81e5f7884
pidajo <99899574+pidajo@users.noreply.github.com>
2022-06-13 19:37:25 +0200

    Create widget.js

---
+++ apps/widbtred/widget.js
@@ -0,0 +1,15 @@
+WIDGETS["btwidred"]={area:"tr",width:19,draw:function() {
+  g.reset();
+  if (NRF.getSecurityStatus().connected)
+    g.setColor((g.getBPP()>8) ? "#07f" : (g.theme.dark ? "#0ff" : "#00f"));
+  else {
+    g.setBgColor("#f00");
+    //g.setColor(g.theme.dark ? "#666" : "#999");
+    g.setColor("#fff");
+  }
+ g.clearRect(this.x + 1, this.y, this.x+17, this.y+25);  g.drawImage(atob("CxQBBgDgFgJgR4jZMawfAcA4D4NYybEYIwTAsBwDAA=="),4+this.x,2+this.y);
+},changed:function() {
+  WIDGETS["btwidred"].draw();
+}};
+NRF.on('connect',WIDGETS["btwidred"].changed);
+NRF.on('disconnect',WIDGETS["btwidred"].changed);
