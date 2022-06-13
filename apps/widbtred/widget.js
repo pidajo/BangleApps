55319fc7a717e5a00c211386c9e1c22dbd8fda46
pidajo <99899574+pidajo@users.noreply.github.com>
2022-06-13 19:40:26 +0200

    Update widget.js

--- apps/widbtred/widget.js
+++ apps/widbtred/widget.js
@@ -1,4 +1,4 @@
-WIDGETS["btwidred"]={area:"tr",width:19,draw:function() {
+WIDGETS["widbtred"]={area:"tr",width:19,draw:function() {
   g.reset();
   if (NRF.getSecurityStatus().connected)
     g.setColor((g.getBPP()>8) ? "#07f" : (g.theme.dark ? "#0ff" : "#00f"));
@@ -9,7 +9,7 @@ WIDGETS["btwidred"]={area:"tr",width:19,draw:function() {
   }
  g.clearRect(this.x + 1, this.y, this.x+17, this.y+25);  g.drawImage(atob("CxQBBgDgFgJgR4jZMawfAcA4D4NYybEYIwTAsBwDAA=="),4+this.x,2+this.y);
 },changed:function() {
-  WIDGETS["btwidred"].draw();
+  WIDGETS["widbtred"].draw();
 }};
-NRF.on('connect',WIDGETS["btwidred"].changed);
-NRF.on('disconnect',WIDGETS["btwidred"].changed);
+NRF.on('connect',WIDGETS["widbtred"].changed);
+NRF.on('disconnect',WIDGETS["widbtred"].changed);
