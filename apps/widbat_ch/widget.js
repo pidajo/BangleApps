3f4135666952822c9ded5e133b876e2a50597d54
pidajo <99899574+pidajo@users.noreply.github.com>
2022-06-16 11:46:45 +0200

    Update widget.js

--- apps/widbat_ch/widget.js
+++ apps/widbat_ch/widget.js
@@ -1,20 +1,21 @@
+if (!global.WIDGETS) {
+  WIDGETS = {};
+  var isTest = true;
+}
 (function(){
-  function setWidth() {
-    WIDGETS["bat"].width = 40 + (Bangle.isCharging()?16:0);
-  }
+  const WIDGET_ID = "widbat_ch";  
   Bangle.on('charging',function(charging) {
     if(charging) Bangle.buzz();
-    setWidth();
     Bangle.drawWidgets(); // re-layout widgets
     g.flip();
   });
-  var batteryInterval = Bangle.isLCDOn() ? setInterval(()=>WIDGETS["bat"].draw(), 60000) : undefined;
+  var batteryInterval = Bangle.isLCDOn() ? setInterval(()=>WIDGETS[WIDGET_ID].draw(), 60000) : undefined;
   Bangle.on('lcdPower', function(on) {
     if (on) {
-      WIDGETS["bat"].draw();
+      WIDGETS[WIDGET_ID].draw();
       // refresh once a minute if LCD on
       if (!batteryInterval)
-        batteryInterval = setInterval(()=>WIDGETS["bat"].draw(), 60000);
+        batteryInterval = setInterval(()=>WIDGETS[WIDGET_ID].draw(), 60000);
     } else {
       if (batteryInterval) {
         clearInterval(batteryInterval);
@@ -22,16 +23,36 @@
       }
     }
   });
-  WIDGETS["bat"]={area:"tr",width:40,draw:function() {
-    var s = 39;
-    var x = this.x, y = this.y;
-    g.reset();
-    if (Bangle.isCharging()) {
-      g.setColor("#0f0").drawImage(atob("DhgBHOBzgc4HOP////////////////////3/4HgB4AeAHgB4AeAHgB4AeAHg"),x,y);
-      x+=16;
+  WIDGETS[WIDGET_ID]={
+    area:"tr",
+    width:40,
+    draw:function() {
+      var s = 39;
+      var x = this.x, y = this.y;
+      g.reset();
+      g.setColor(g.theme.fg).fillRect(x,y+2,x+s-4,y+21).clearRect(x+2,y+4,x+s-6,y+19).fillRect(x+s-3,y+10,x+s,y+14);
+      g.setColor("#0f0");
+      var bat = E.getBattery();
+      bat = 10;
+      /*
+      if (bat < 40) {
+        var c = bat - 15;
+        g.setColor(1 - c / 25.0, c / 25.0, 0);
+      }
+      */
+      if (bat < 30) {
+        g.setColor("#f80");
+      }
+      if (bat < 15) {
+        g.setColor("#f00");
+      }
+      if (Bangle.isCharging()) {
+        g.setColor("#44f")
+      }
+      g.fillRect(x+4,y+6,x+4+bat*(s-12)/100,y+17);
     }
-    g.setColor(g.theme.fg).fillRect(x,y+2,x+s-4,y+21).clearRect(x+2,y+4,x+s-6,y+19).fillRect(x+s-3,y+10,x+s,y+14);
-    g.setColor("#0f0").fillRect(x+4,y+6,x+4+E.getBattery()*(s-12)/100,y+17);
-  }};
-  setWidth();
+  };
 })()
+if (global.isTest) {
+  Bangle.drawWidgets();
+}
